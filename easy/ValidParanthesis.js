73fac318badc4f326c6b3089d2d22ba4ee36e530
Vijaya Laxmi <vijaya2798@gmail.com>
2023-10-14 16:47:43 +0530

    format

--- easy/ValidParanthesis.js
+++ easy/ValidParanthesis.js
@@ -4,19 +4,19 @@
  * Open brackets must be closed by the same type of brackets.
  * Open brackets must be closed in the correct order.
  * Every close bracket has a corresponding open bracket of the same type.
- * 
+ *
  * Example 1:
  * Input: s = "()"
  * Output: true
- * 
+ *
  * Example 2:
  * Input: s = "()[]{}"
  * Output: true
- * 
+ *
  * Example 3:
  * Input: s = "(]"
  * Output: false
- * 
+ *
  * Constraints:
  * 1 <= s.length <= 104
  * s consists of parentheses only '()[]{}'.
@@ -25,23 +25,21 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    let matchMap = new Map ();
-    matchMap.set('(', ')');
-    matchMap.set('[', ']');
-    matchMap.set('{', '}');
-    let stack = [];
-    for(let i=0; i<s.length; i++){
-        if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
-            stack.push(s[i]);
-        }
-        else{
-            let top = stack[stack.length-1];
-            if(matchMap.get(top) == s[i]){
-                stack.pop();
-            }
-            else return false;
-        }
+var isValid = function (s) {
+  let matchMap = new Map();
+  matchMap.set("(", ")");
+  matchMap.set("[", "]");
+  matchMap.set("{", "}");
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == "(" || s[i] == "[" || s[i] == "{") {
+      stack.push(s[i]);
+    } else {
+      let top = stack[stack.length - 1];
+      if (matchMap.get(top) == s[i]) {
+        stack.pop();
+      } else return false;
     }
-    return !stack.length
+  }
+  return !stack.length;
 };
